5b6ac4b0fd39340325b39ddfb2034338c42de26a
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-05-24 20:00:24 -0400

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false,
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // insert new doc into Users (name, age, location)
+
+  // db.collection('Users').insertOne({
+  //   name: 'Ryan',
+  //   age: 30,
+  //   location: 'Washington, DC',
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to add user', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
+  db.close();
+});
