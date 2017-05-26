8f3df05bee6017a7670f31115ecfdb77264553bd
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-05-26 00:19:59 -0400

    Add Get /todos/:id

--- server/svr.js
+++ server/svr.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -25,6 +26,23 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+     return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    return res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
