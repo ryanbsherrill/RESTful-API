aa4f4c06e6e315939ffaf0cc69a505a245b0c884
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-06-06 01:03:14 -0400

    Add tests for DELETE /users/me/token

--- server/tests/svr.test.js
+++ server/tests/svr.test.js
@@ -252,3 +252,22 @@ describe('POST/ users', () => {
       .end(done);
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', testUsers[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(testUsers[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
