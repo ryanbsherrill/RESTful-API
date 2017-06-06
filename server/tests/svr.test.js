32b95a5f77be1ff9a283dd988d54cababfb3fcb6
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-06-06 03:40:29 -0400

    Add tests for POST /users/login

--- server/tests/svr.test.js
+++ server/tests/svr.test.js
@@ -202,7 +202,7 @@ describe('GET/ users/me', () => {
   });
 });
 
-// POST USERS TESTS
+// POST USERS TESTS => SIGNUP
 describe('POST/ users', () => {
   it('should create a user', (done) => {
     let email = 'example@example.com';
@@ -226,7 +226,7 @@ describe('POST/ users', () => {
           expect(user).toExist();
           expect(user.password).toNotBe(password);
           done();
-        });
+        }).catch((e) => done(e));
       });
   });
 
@@ -253,6 +253,60 @@ describe('POST/ users', () => {
   });
 });
 
+// POST USERS TESTS => LOGIN
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: testUsers[1].email,
+        password: testUsers[1].password,
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(testUsers[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: testUsers[1].email,
+        password: testUsers[1].password + '1',
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(testUsers[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
+
+
+// DELETE USERS ROUTE
 describe('DELETE /users/me/token', () => {
   it('should remove auth token on logout', (done) => {
     request(app)
